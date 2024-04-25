2eae5b58459398d2b715a40d01e12639f0767467
Saikrishna41 <saai.krrishna@gmail.com>
2024-04-25 17:18:41 +0530

    Add splash screen api

--- app/src/main/java/com/techfortyone/newspeekv1/MainActivity.kt
+++ app/src/main/java/com/techfortyone/newspeekv1/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.techfortyone.newspeekv1.ui.theme.NewsPeekV1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsPeekV1Theme {
                 // A surface container using the 'background' color from the theme
